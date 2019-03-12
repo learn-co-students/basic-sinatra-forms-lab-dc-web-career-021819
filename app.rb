e86f37e2e531a80f1b79230b88339a03926be0c0
Mickey <micksheridan.24@gmail.com>
2019-03-12 14:25:26 -0400

    submit

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 class App < Sinatra::Base
+  get "/newteam" do
+    erb :newteam
+  end
 
-
+  post "/team" do
+    @name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @c = params[:c]
+    @pf = params[:pf]
+    @coach = params[:coach]
+    @sf = params[:sf]
+    erb :team
+  end
 end
